2950f34fa759194e4ecef7696d180e9cb73677d8
seaisbig <101154420+seaisbig@users.noreply.github.com>
2022-03-11 15:19:28 +0800

    Delete hello-service directory

--- hello-service/src/main/java/edu/zut/hb/software/dream/hello/service/HelloManager.java
+++
@@ -1,4 +0,0 @@
-package edu.zut.hb.software.dream.hello.service;
-public interface HelloManager {
-String sayHello(String name);
-}
--- hello-service/src/main/java/edu/zut/hb/software/dream/hello/service/impl/HelloManagerImpl.java
+++
@@ -1,9 +0,0 @@
-package edu.zut.hb.software.dream.hello.service.impl;
-import edu.zut.hb.software.dream.hello.service.HelloManager;
-public class HelloManagerImpl implements HelloManager {
-    @Override
-    public String sayHello(String name) {
-       String result="Hello,"+name+"!";
-       return result;
-    }
-}
